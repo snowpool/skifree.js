7f66fffcefd683e6bc4395c1f0a2f4cbb62ac38c
Patrick Davey <patrick.davey@resolvedigital.co.nz>
2013-02-20 18:21:07 +1300

    altering directory structure and html for snowpool

---
+++ ski_free_js/main.js
@@ -0,0 +1,355 @@
+var mainCanvas = document.getElementById('skifree-canvas');
+var dContext = mainCanvas.getContext('2d');
+var imageSources = [ 'sprite-characters.png', 'skifree-objects.png' ];
+var global = this;
+var infoBoxControls = 'Use the mouse or WASD to control the skier';
+if (isMobileDevice()) infoBoxControls = 'Tap or drag on the piste to control the skier';
+var SpriteArray = require('spriteArray');
+var Monster = require('monster');
+var Sprite = require('sprite');
+var Snowboarder = require('snowboarder');
+var Skier = require('skier');
+var InfoBox = require('infoBox');
+var sprites = require('spriteInfo');
+
+var pixelsPerMetre = 18;
+var monstersComeOut = false;
+var distanceTravelledInMetres = 0;
+var livesLeft = 5;
+var highScore = 0;
+if (localStorage.getItem('highScore')) highScore = localStorage.getItem('highScore');
+
+function loadImages (sources, next) {
+	var loaded = 0;
+	var images = {};
+
+	function finish () {
+		loaded += 1;
+		if (loaded === sources.length) {
+			next(images);
+		}
+	}
+
+	sources.each(function (src) {
+		var im = new Image();
+		im.onload = finish;
+		im.src = src;
+		dContext.storeLoadedImage(src, im);
+	});
+}
+
+function monsterHitsSkierBehaviour(monster, skier) {
+	skier.isEatenBy(monster, function () {
+		livesLeft -= 1;
+		monster.isFull = true;
+		monster.isEating = false;
+		skier.isBeingEaten = false;
+		monster.setSpeed(skier.getSpeed());
+		monster.setMapPositionTargetWithConviction(dContext.getRandomlyInTheCentreOfCanvas(), dContext.getAboveViewport());
+	});
+}
+
+function startGame (images) {
+	var skier;
+	var startSign;
+	var infoBox;
+	var staticObjects = new SpriteArray();
+	var monsters = [];
+	var snowboarders = [];
+	var jumps = [];
+	var thickSnowPatches = [];
+	var mouseX = dContext.getCentreOfViewport();
+	var mouseY = 0;
+	var paused = false;
+
+	function resetGame () {
+		pixelsPerMetre = 18;
+		monstersComeOut = false;
+		distanceTravelledInMetres = 0;
+		livesLeft = 5;
+		highScore = localStorage.getItem('highScore');
+		monsters = [];
+		skier.reset();
+		skier.setMapPosition(0, 0, 0);
+	}
+
+	function detectEnd () {
+		paused = true;
+		highScore = localStorage.setItem('highScore', distanceTravelledInMetres);
+		console.log('Game over!');
+	}
+
+	function randomlyGenerateObject(sprite, dropRate) {
+		var rateModifier = Math.max(800 - mainCanvas.width, 0);
+		if (Number.random(100 + rateModifier) <= dropRate && skier.isMoving) {
+			var newSprite = new Sprite(sprite);
+			newSprite.setSpeed(0);
+			var randomPosition = dContext.getRandomMapPositionBelowViewport();
+			newSprite.setMapPosition(randomPosition[0], randomPosition[1]);
+
+			if (sprite.hitBehaviour.monster) {
+				monsters.each(function (monster) {
+					newSprite.onHitting(monster, sprite.hitBehaviour.monster);
+				});
+			}
+
+			if (sprite.hitBehaviour.skier) {
+				newSprite.onHitting(skier, sprite.hitBehaviour.skier);
+			}
+
+			staticObjects.push(newSprite);
+		}
+	}
+
+	function spawnMonster () {
+		var newMonster = new Monster(sprites.monster);
+		var randomPosition = dContext.getRandomMapPositionAboveViewport();
+		newMonster.setMapPosition(randomPosition[0], randomPosition[1]);
+		newMonster.follow(skier);
+		newMonster.onHitting(skier, monsterHitsSkierBehaviour);
+
+		staticObjects.each(function (obj) {
+			if (obj.data.hitBehaviour.monster) {
+				obj.onHitting(newMonster, obj.data.hitBehaviour.monster);
+			}
+		});
+
+		staticObjects.onPush(function (obj) {
+			if (obj.data.hitBehaviour.monster) {
+				obj.onHitting(newMonster, obj.data.hitBehaviour.monster);
+			}
+		});
+
+		monsters.push(newMonster);
+	}
+
+	function spawnBoarder () {
+		var newBoarder = new Snowboarder(sprites.snowboarder);
+		var randomPositionAbove = dContext.getRandomMapPositionAboveViewport();
+		var randomPositionBelow = dContext.getRandomMapPositionBelowViewport();
+		newBoarder.setMapPosition(randomPositionAbove[0], randomPositionAbove[1]);
+		newBoarder.setMapPositionTarget(randomPositionBelow[0], randomPositionBelow[1]);
+		newBoarder.onHitting(skier, sprites.snowboarder.hitBehaviour.skier);
+
+		snowboarders.push(newBoarder);
+	}
+
+	startSign = new Sprite(sprites.signStart);
+	startSign.setMapPosition(-50, 0);
+
+	skier = new Skier(sprites.skier);
+	skier.setMapPosition(0, 0);
+	skier.setMapPositionTarget(0, -10);
+	dContext.followSprite(skier);
+
+	infoBox = new InfoBox({
+		initialLines : [
+			'SkiFree.js',
+			infoBoxControls,
+			'Travelled 0m',
+			'High Score: ' + highScore,
+			'Skiers left: ' + livesLeft,
+			'Original Creation by Dan Hough (@basicallydan)',
+			'Co-opted by Snowpool for Coolness!'
+
+		],
+		position: {
+			top: 15,
+			right: 10
+		}
+	});
+
+	var intervalNum = 0;
+
+	setInterval(function () {
+		// Clear canvas
+		mainCanvas.width = mainCanvas.width;
+		var mouseMapPosition = dContext.canvasPositionToMapPosition([mouseX, mouseY]);
+
+		if (!skier.isJumping) {
+			if (intervalNum === 0) {
+				console.log('Skier targets: ' + skier.movingToward[0] + ', ' + skier.movingToward[1]);
+			}
+			skier.setMapPositionTarget(mouseMapPosition[0], mouseMapPosition[1]);
+			if (intervalNum === 0) {
+				console.log('Skier targets: ' + skier.movingToward[0] + ', ' + skier.movingToward[1]);
+			}
+		}
+
+		intervalNum++;
+
+		startSign.draw(dContext, 'main');
+		skier.draw(dContext);
+
+		monsters.each(function (monster, i) {
+			if (monster.isAboveOnCanvas(dContext.getAboveViewport() - 100) || monster.deleted) {
+				return (delete monsters[i]);
+			}
+
+			if (monster.isFull) {
+				monster.stopFollowing();
+			} else if (!skier.isBeingEaten) {
+				monster.follow(skier);
+			} else if (skier.isBeingEaten && !monster.isEating) {
+				monster.stopFollowing();
+				monster.setMapPositionTargetWithConviction(dContext.getRandomlyInTheCentreOfCanvas(), dContext.getAboveViewport());
+			}
+
+			monster.cycle();
+			monster.draw(dContext);
+		});
+
+		snowboarders.each(function (snowboarder, i) {
+			if (snowboarder.isBelowOnCanvas(dContext.getBelowViewport() + 499)) {
+				console.log('Deleting snowboarder');
+				return (delete snowboarders[i]);
+			}
+
+			snowboarder.cycle(dContext);
+			snowboarder.draw(dContext);
+		});
+
+		randomlyGenerateObject(sprites.smallTree, 4);
+		randomlyGenerateObject(sprites.tallTree, 2);
+		randomlyGenerateObject(sprites.jump, 1);
+		randomlyGenerateObject(sprites.thickSnow, 1);
+		randomlyGenerateObject(sprites.rock, 1);
+
+		distanceTravelledInMetres = parseFloat(skier.getPixelsTravelledDownMountain() / pixelsPerMetre).toFixed(1);
+
+		if (!monstersComeOut && distanceTravelledInMetres >= 1000) {
+			monstersComeOut = true;
+		}
+ 
+		if (monstersComeOut && Number.random(1600) === 1) {
+			spawnMonster();
+		}
+ 
+		if (Number.random(1300) === 1) {
+			spawnBoarder();
+		}
+
+
+		skier.cycle();
+		
+		staticObjects.cull();
+
+		staticObjects.each(function (staticObject, i) {
+			staticObject.cycle();
+			staticObject.draw(dContext, 'main');
+
+			if (staticObject.isAboveOnCanvas(0)) {
+				staticObject.deleteOnNextCycle();
+			}
+		});
+
+		infoBox.setLines([
+			'SkiFree.js',
+			infoBoxControls,
+			'Travelled ' + distanceTravelledInMetres + 'm',
+			'Skiers left: ' + livesLeft,
+			'High Score: ' + highScore,
+			'Original Creation by Dan Hough (@basicallydan)',
+			'Co-opted by Snowpool for Coolness!',
+			'Current Speed: ' + skier.getSpeed()/*,
+			'Skier Map Position: ' + skier.mapPosition[0].toFixed(1) + ', ' + skier.mapPosition[1].toFixed(1),
+			'Mouse Map Position: ' + mouseMapPosition[0].toFixed(1) + ', ' + mouseMapPosition[1].toFixed(1)*/
+		]);
+
+		infoBox.draw(dContext);
+
+		if (livesLeft === 0) {
+			detectEnd();
+			resetGame();
+		}
+	}, 10);
+	
+	$(mainCanvas)
+	.mousemove(function (e) {
+		mouseX = e.pageX;
+		mouseY = e.pageY;
+	})
+	.bind('click', function (e) {
+		mouseX = e.pageX;
+		mouseY = e.pageY;
+	})
+	.bind('keydown', function (e) {
+		// F Key
+		if (e.keyCode === 70 || e.keyCode === 102) {
+			skier.speedBoost();
+		}
+		//W Key
+		if (e.Keycode === 87 || e.keyCode === 119 || e.keyCode === 38) {
+			mouseX = 0;
+			mouseY = 0;
+		}
+		// A Key
+		if (e.keyCode === 65 || e.keyCode === 97 || e.keyCode === 37) {
+			mouseX = 0;
+			mouseY = mainCanvas.height;
+		}
+		// S Key
+		if (e.keyCode === 83 || e.keyCode === 115 || e.keyCode === 40) {
+			mouseX = mainCanvas.width / 2;
+			mouseY = mainCanvas.height;
+		}
+		// D Key
+		if (e.keyCode === 68 || e.keyCode === 100 || e.keyCode === 39) {
+			mouseX = mainCanvas.width;
+			mouseY = mainCanvas.height;
+		}
+		// M key
+		if (e.keyCode === 77 || e.keyCode === 109) {
+			spawnMonster();
+		}
+		// B key
+		if (e.keyCode === 66 || e.keyCode === 98) {
+			spawnBoarder();
+		}
+	})
+	.hammer({})
+	.bind('hold', function (e) {
+		mouseX = e.position[0].x;
+		mouseY = e.position[0].y;
+	})
+	.bind('tap', function (e) {
+		mouseX = e.position[0].x;
+		mouseY = e.position[0].y;
+	})
+	.bind('drag', function (e) {
+		mouseX = e.position.x;
+		mouseY = e.position.y;
+	})
+	.bind('doubletap', function (e) {
+		skier.speedBoost();
+	})
+	// Focus on the canvas so we can listen for key events immediately
+	.focus();
+}
+
+function resizeCanvas() {
+	mainCanvas.width = window.innerWidth;
+	mainCanvas.height = window.innerHeight;
+}
+
+function isMobileDevice() {
+	if(navigator.userAgent.match(/Android/i) ||
+		navigator.userAgent.match(/webOS/i) ||
+		navigator.userAgent.match(/iPhone/i) ||
+		navigator.userAgent.match(/iPad/i) ||
+		navigator.userAgent.match(/iPod/i) ||
+		navigator.userAgent.match(/BlackBerry/i) ||
+		navigator.userAgent.match(/Windows Phone/i)
+	) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+window.addEventListener('resize', resizeCanvas, false);
+
+resizeCanvas();
+
+loadImages(imageSources, startGame);
---
+++ ski_free_js/skier.js
@@ -0,0 +1,323 @@
+var Sprite = require('./Sprite');
+
+(function(global) {
+	function Skier(data) {
+		var that = new Sprite(data);
+		var sup = {
+			draw: that.superior('draw'),
+			cycle: that.superior('cycle'),
+			getSpeedX: that.superior('getSpeedX'),
+			getSpeedY: that.superior('getSpeedY'),
+			hits: that.superior('hits')
+		};
+		var directions = {
+			esEast: function(xDiff) { return xDiff > 300; },
+			sEast: function(xDiff) { return xDiff > 75; },
+			wsWest: function(xDiff) { return xDiff < -300; },
+			sWest: function(xDiff) { return xDiff < -75; }
+		};
+
+		var cancelableStateTimeout;
+
+		var canSpeedBoost = true;
+
+		var obstaclesHit = [];
+		var pixelsTravelled = 0;
+		var standardSpeed = 5;
+		var turnEaseCycles = 70;
+		var speedX = 0;
+		var speedXFactor = 0;
+		var speedY = 0;
+		var speedYFactor = 1;
+
+		that.isMoving = true;
+		that.hasBeenHit = false;
+		that.isJumping = false;
+		that.setSpeed(standardSpeed);
+
+		that.reset = function () {
+			obstaclesHit = [];
+			pixelsTravelled = 0;
+			that.isMoving = true;
+			that.hasBeenHit = false;
+			canSpeedBoost = true;
+		};
+
+		function setNormal() {
+			that.setSpeed(standardSpeed);
+			that.isMoving = true;
+			that.hasBeenHit = false;
+			that.isJumping = false;
+			that.setMapPosition(undefined, undefined, 0);
+		}
+
+		function setCrashed() {
+			that.isMoving = false;
+			that.hasBeenHit = true;
+			that.isJumping = false;
+			that.setMapPosition(undefined, undefined, 0);
+		}
+
+		function setJumping() {
+			var currentSpeed = that.getSpeed();
+			that.setSpeed(currentSpeed + 2);
+			that.setSpeedY(currentSpeed + 2);
+			that.isMoving = true;
+			that.hasBeenHit = false;
+			that.isJumping = true;
+			that.setMapPosition(undefined, undefined, 1);
+		}
+
+		function getDirection() {
+			var xDiff = that.movingToward[0] - that.mapPosition[0];
+			var yDiff = that.movingToward[1] - that.mapPosition[1];
+			if (yDiff <= 0) {
+				if (xDiff > 0) {
+					return 'east';
+				} else {
+					return 'west';
+				}
+			}
+
+			if (directions.esEast(xDiff)) {
+				return 'esEast';
+			} else if (directions.sEast(xDiff)) {
+				return 'sEast';
+			} else if (directions.wsWest(xDiff)) {
+				return 'wsWest';
+			} else if (directions.sWest(xDiff)) {
+				return 'sWest';
+			}
+			return 'south';
+		}
+
+		function getBeingEatenSprite() {
+			return 'blank';
+		}
+
+		function getJumpingSprite() {
+			return 'jumping';
+		}
+
+		that.setMapPositionTarget = function (x, y) {
+			if (that.hasBeenHit) return;
+
+			if (y > that.mapPosition[1]) {
+				that.isMoving = true;
+			} else {
+				that.isMoving = false;
+				// y = that.mapPosition[1];
+			}
+
+			if (Math.abs(that.mapPosition[0] - x) <= 75) {
+				x = that.mapPosition[0];
+			}
+
+			that.movingToward = [ x, y ];
+		};
+
+		that.setTurnEaseCycles = function (c) {
+			turnEaseCycles = c;
+		};
+
+		that.getPixelsTravelledDownMountain = function () {
+			return pixelsTravelled;
+		};
+
+		that.resetSpeed = function () {
+			that.setSpeed(standardSpeed);
+		};
+
+		that.cycle = function () {
+			if (that.isMoving) {
+				pixelsTravelled += that.speed;
+			}
+
+			if (that.isJumping) {
+				that.setMapPositionTarget(undefined, that.mapPosition[1] + that.getSpeed());
+			}
+
+			sup.cycle();
+			
+			that.checkHittableObjects();
+		};
+
+		that.draw = function(dContext) {
+			var spritePartToUse = function () {
+				if (that.isBeingEaten) {
+					return getBeingEatenSprite();
+				}
+
+				if (that.isJumping) {
+					return getJumpingSprite();
+				}
+
+				if (that.hasBeenHit) {
+					return 'hit';
+				}
+
+				return getDirection();
+			};
+
+			return sup.draw(dContext, spritePartToUse());
+		};
+
+		that.hits = function (obs) {
+			if (obstaclesHit.indexOf(obs.id) !== -1) {
+				return false;
+			}
+
+			if (!obs.occupiesZIndex(that.mapPosition[2])) {
+				return false;
+			}
+
+			if (sup.hits(obs)) {
+				return true;
+			}
+
+			return false;
+		};
+
+		that.speedBoost = function () {
+			var originalSpeed = that.speed;
+			if (canSpeedBoost) {
+				canSpeedBoost = false;
+				that.setSpeed(that.speed * 2);
+				setTimeout(function () {
+					that.setSpeed(originalSpeed);
+					setTimeout(function () {
+						canSpeedBoost = true;
+					}, 10000);
+				}, 2000);
+			}
+		};
+
+		function easeSpeedToTargetUsingFactor(sp, targetSpeed, f) {
+			if (f === 0 || f === 1) {
+				return targetSpeed;
+			}
+
+			if (sp < targetSpeed) {
+				sp += that.getSpeed() * (f / turnEaseCycles);
+			}
+
+			if (sp > targetSpeed) {
+				sp -= that.getSpeed() * (f / turnEaseCycles);
+			}
+
+			return sp;
+		}
+
+		that.getSpeedX = function () {
+			if (getDirection() === 'esEast' || getDirection() === 'wsWest') {
+				speedXFactor = 0.5;
+				speedX = easeSpeedToTargetUsingFactor(speedX, that.getSpeed() * speedXFactor, speedXFactor);
+
+				return speedX;
+			}
+
+			if (getDirection() === 'sEast' || getDirection() === 'sWest') {
+				speedXFactor = 0.33;
+				speedX = easeSpeedToTargetUsingFactor(speedX, that.getSpeed() * speedXFactor, speedXFactor);
+
+				return speedX;
+			}
+
+			// So it must be south
+
+			speedX = easeSpeedToTargetUsingFactor(speedX, 0, speedXFactor);
+
+			return speedX;
+		};
+
+		that.setSpeedY = function(sy) {
+			speedY = sy;
+		};
+
+		that.getSpeedY = function () {
+			var targetSpeed;
+
+			if (that.isJumping) {
+				return speedY;
+			}
+
+			if (getDirection() === 'esEast' || getDirection() === 'wsWest') {
+				speedYFactor = 0.6;
+				speedY = easeSpeedToTargetUsingFactor(speedY, that.getSpeed() * 0.6, 0.6);
+
+				return speedY;
+			}
+
+			if (getDirection() === 'sEast' || getDirection() === 'sWest') {
+				speedYFactor = 0.85;
+				speedY = easeSpeedToTargetUsingFactor(speedY, that.getSpeed() * 0.85, 0.85);
+
+				return speedY;
+			}
+
+			if (getDirection() === 'east' || getDirection() === 'west') {
+				speedYFactor = 1;
+				speedY = 0;
+
+				return speedY;
+			}
+
+			// So it must be south
+
+			speedY = easeSpeedToTargetUsingFactor(speedY, that.getSpeed(), speedYFactor);
+
+			return speedY;
+		};
+
+		that.hasHitObstacle = function (obs) {
+			setCrashed();
+
+			obstaclesHit.push(obs.id);
+
+			that.resetSpeed();
+
+			if (cancelableStateTimeout) {
+				clearTimeout(cancelableStateTimeout);
+			}
+			cancelableStateTimeout = setTimeout(function() {
+				setNormal();
+			}, 1500);
+		};
+
+		that.hasHitJump = function () {
+			setJumping();
+
+			if (cancelableStateTimeout) {
+				clearTimeout(cancelableStateTimeout);
+			}
+			cancelableStateTimeout = setTimeout(function() {
+				setNormal();
+			}, 1000);
+		};
+
+		that.isEatenBy = function (monster, whenEaten) {
+			that.hasHitObstacle(monster);
+			monster.startEating(whenEaten);
+			obstaclesHit.push(monster.id);
+			that.isMoving = false;
+			that.isBeingEaten = true;
+		};
+
+		that.reset = function () {
+			obstaclesHit = [];
+			pixelsTravelled = 0;
+			that.isMoving = true;
+			that.isJumping = false;
+			that.hasBeenHit = false;
+			canSpeedBoost = true;
+		};
+
+		return that;
+	}
+
+	global.skier = Skier;
+})(this);
+
+if (typeof module !== 'undefined') {
+	module.exports = this.skier;
+}
